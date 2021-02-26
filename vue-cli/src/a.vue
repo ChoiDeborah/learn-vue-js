017e1166b3aa923e30159efdf2eddcf5f27a2304
ChoiDeborah <adorablejimini@gmail.com>
2021-02-26 17:56:56 +0900

    App.vue와 HelloWorld.vue 설명

--- vue-cli/src/a.vue
+++ vue-cli/src/a.vue
@@ -1,13 +1,19 @@
 <template>
+    <!-- HTML -->
     <div>header</div>
 </template>
 
 <script>
 export default {
+    methods: {
+        // Javascript - 인스턴스 옵션
+        addNum: function() {
 
+        }
+    }
 }
 </script>
 
 <style>
-
+    /* CSS */
 </style>
